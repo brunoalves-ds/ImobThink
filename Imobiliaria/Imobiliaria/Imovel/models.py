767319e1bf19494372faa81aff3ae925c195967f
raajow <mrwilliamdias@gmail.com>
2019-10-30 21:01:45 -0300

    Templates Imovel VIEW Atualizados

--- Imobiliaria/Imobiliaria/Imovel/models.py
+++ Imobiliaria/Imobiliaria/Imovel/models.py
@@ -21,6 +21,7 @@ class imovel(models.Model):
     andares = models.PositiveSmallIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    descricao = models.TextField(null=True)
     foto1 = models.ImageField(upload_to='foto_imovel', null=True, blank=True)
     foto2 = models.ImageField(upload_to='foto_imovel', null=True, blank=True)
     foto3 = models.ImageField(upload_to='foto_imovel', null=True, blank=True)
